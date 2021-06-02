57bb070560a3f9171efc0dead63148d5c65e3fd1
BogdanFi <bogdan_star1@yahoo.com>
2021-06-02 09:10:46 +0300

    Update v3

--- Chess/Form1.Designer.cs
+++ Chess/Form1.Designer.cs
@@ -29,25 +29,45 @@
         private void InitializeComponent()
         {
             this.btn_JocNou = new System.Windows.Forms.Button();
+            this.btn_IncarcareJoc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_JocNou
             // 
+            this.btn_JocNou.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(30)))));
+            this.btn_JocNou.FlatAppearance.BorderSize = 0;
+            this.btn_JocNou.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_JocNou.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_JocNou.ForeColor = System.Drawing.Color.White;
             this.btn_JocNou.Location = new System.Drawing.Point(313, 111);
             this.btn_JocNou.Name = "btn_JocNou";
-            this.btn_JocNou.Size = new System.Drawing.Size(141, 23);
+            this.btn_JocNou.Size = new System.Drawing.Size(141, 40);
             this.btn_JocNou.TabIndex = 0;
-            this.btn_JocNou.Text = "Joc Nou";
-            this.btn_JocNou.UseVisualStyleBackColor = true;
+            this.btn_JocNou.Text = "Joc nou";
+            this.btn_JocNou.UseVisualStyleBackColor = false;
             this.btn_JocNou.Click += new System.EventHandler(this.btn_JocNou_Click);
-            
+            // 
+            // btn_IncarcareJoc
+            // 
+            this.btn_IncarcareJoc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(30)))));
+            this.btn_IncarcareJoc.FlatAppearance.BorderSize = 0;
+            this.btn_IncarcareJoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_IncarcareJoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_IncarcareJoc.ForeColor = System.Drawing.Color.White;
+            this.btn_IncarcareJoc.Location = new System.Drawing.Point(313, 199);
+            this.btn_IncarcareJoc.Name = "btn_IncarcareJoc";
+            this.btn_IncarcareJoc.Size = new System.Drawing.Size(141, 40);
+            this.btn_IncarcareJoc.TabIndex = 1;
+            this.btn_IncarcareJoc.Text = "Încărcare partidă";
+            this.btn_IncarcareJoc.UseVisualStyleBackColor = false;
+            this.btn_IncarcareJoc.Click += new System.EventHandler(this.btn_IncarcareJoc_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_IncarcareJoc);
             this.Controls.Add(this.btn_JocNou);
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -60,6 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Button btn_JocNou;
+        private System.Windows.Forms.Button btn_IncarcareJoc;
     }
 }
 
